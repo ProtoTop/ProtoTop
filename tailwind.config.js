080dae7c4a65a7eb119c78787f6a39f94d4182a5
johnbabu021 <johnbabu021@gmail.com>
2022-01-18 07:57:50 +0530

    fix:tailwind configuration error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  content:[
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/utils/**/*.{js,ts,jsx,tsx}",
+    "./components/core/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
      extend: {},
   },
